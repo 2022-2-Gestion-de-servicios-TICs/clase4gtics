c9f0b90d2659d1083592d085eb408a8f998f58c2
stuardo asus-tuf <stuardo.lucho@pucp.edu.pe>
2022-09-05 10:37:28 -0500

    código desarrollado en clase 4

--- src/main/java/com/example/clase4gtics/controller/ProductController.java
+++ src/main/java/com/example/clase4gtics/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.example.clase4gtics.controller;
 
 import com.example.clase4gtics.entity.Product;
-import com.example.clase4gtics.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.example.clase4gtics.repository.*;
 
 import java.util.Optional;
 
@@ -20,6 +20,12 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    SupplierRepository supplierRepository;
+
     @GetMapping(value = {"", "/"})
     public String listaProductos(Model model) {
         model.addAttribute("listaProductos", productRepository.findAll());
@@ -27,7 +33,9 @@ public class ProductController {
     }
 
     @GetMapping("/new")
-    public String nuevoProductoFrm() {
+    public String nuevoProductoFrm(Model model) {
+        model.addAttribute("listaCategorias", categoryRepository.findAll());
+        model.addAttribute("listaProveedores", supplierRepository.findAll());
         return "product/newFrm";
     }
 
@@ -38,13 +46,15 @@ public class ProductController {
     }
 
     @GetMapping("/edit")
-    public String editarTransportista(Model model,@RequestParam("id") int id) {
+    public String editarTransportista(Model model, @RequestParam("id") int id) {
 
         Optional<Product> optProduct = productRepository.findById(id);
 
         if (optProduct.isPresent()) {
             Product product = optProduct.get();
             model.addAttribute("product", product);
+            model.addAttribute("listaCategorias", categoryRepository.findAll());
+            model.addAttribute("listaProveedores", supplierRepository.findAll());
             return "product/editFrm";
         } else {
             return "redirect:/product";
